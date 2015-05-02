530e36be66634790fdb4a8d7970a44e5b3b077dd
Michael Bromley <michael@michaelbromley.co.uk>
2015-05-02 22:26:20 +0200

    Add GUI for configurable params

--- visualizations/basic.js
+++ visualizations/basic.js
@@ -4,27 +4,43 @@ function basic(skqw) {
         ft,
         ts,
         w,
-        h;
+        h,
+        params = {
+            sensitivity: 250
+        },
+        paramsMetadata = {
+            sensitivity : {
+                min: 0,
+                max: 500,
+                step: 1
+            }
+        };
 
     return {
         name: 'A Basic Visualization',
         author: 'Michael Bromley',
-        init: function() {
-            ctx = skqw.createCanvas().getContext('2d');
-        },
-        tick: function(timestamp) {
-            w = skqw.container.offsetWidth;
-            h = skqw.container.offsetHeight;
-            ft = skqw.stream.ft;
-            ts = skqw.stream.ts;
-
-            ctx.fillStyle = 'rgba(255, 255, 255, 0.1)';
-            ctx.fillRect(0, 0, w, h);
-            ctx.fillStyle = 'rgba(0, 0, 0, 0.8)';
-            drawBars();
-            drawWave();
-        }
+        params: params,
+        paramsMetadata: paramsMetadata,
+        init: init,
+        tick: tick
     };
+
+    function init() {
+        ctx = skqw.createCanvas().getContext('2d');
+    }
+
+    function tick(timestamp) {
+        w = skqw.container.offsetWidth;
+        h = skqw.container.offsetHeight;
+        ft = skqw.stream.ft;
+        ts = skqw.stream.ts;
+
+        ctx.fillStyle = 'rgba(255, 255, 255, 0.1)';
+        ctx.fillRect(0, 0, w, h);
+        ctx.fillStyle = 'rgba(0, 0, 0, 0.8)';
+        drawBars();
+        drawWave();
+    }
 
     function drawWave() {
         var length = ts.length,
@@ -32,7 +48,7 @@ function basic(skqw) {
 
         ts.forEach(function(val, i) {
             var x = i * width,
-                y = h / 2 + val * 500;
+                y = h / 2 + val * params.sensitivity * 10;
 
             ctx.fillRect(x, y, 1, 1);
         });
@@ -45,8 +61,8 @@ function basic(skqw) {
         for(var i = 0; i < ft.length; i++) {
             var val = ft[i],
                 x = i * width,
-                y = 0,
-                height = val * 10;
+                height = val * params.sensitivity,
+                y = h - height;
 
             ctx.fillRect(x, y, width, height);
         }
