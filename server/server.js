5dc5ace0ba4e6aa3d673516ecc3092907057519c
Michael Bromley <michael@michaelbromley.co.uk>
2015-05-31 22:02:48 +0200

    Add debug code to measure sample frequency

--- server/server.js
+++ server/server.js
@@ -11,8 +11,13 @@ var coreAudio = require('node-core-audio'),
     BUFFER_SIZE = 256,
 
     engine = coreAudio.createNewAudioEngine(),
+    inputBuffer,
     fft = new FFT.complex( BUFFER_SIZE / 2, false ),
     fftBuffer = new Float32Array(BUFFER_SIZE);
+
+// debug - calculate frequency of audiocallback calls
+var fpsFilter = 50;
+var fps = 0, now, lastUpdate = (new Date)*1;
 
 engine.setOptions({
     framesPerBuffer: BUFFER_SIZE,
@@ -67,10 +72,31 @@ function appendToElement(element) {
  *
  * @param {Array} inputBuffer
  */
-function processAudio(inputBuffer) {
+function processAudio(_inputBuffer) {
+
+    if (!inputBuffer) {
+        setTimeout(sendSample, 20);
+    }
+
+    inputBuffer = _inputBuffer;
+
+    var thisFrameFPS = 1000 / ((now=new Date*1) - lastUpdate);
+    if (now!=lastUpdate){
+        fps += (thisFrameFPS - fps) / fpsFilter;
+        lastUpdate = now;
+    }
+    if (now % 10 === 0) {
+        console.log('socket frequency: ' + fps);
+    }
+}
+
+
+function sendSample() {
     var ts = new Float32Array(inputBuffer[0]), ft;
     fft.simple(fftBuffer, ts, 'complex');
     ft = fftBuffer.slice(0, BUFFER_SIZE / 2);
 
     io.emit('sample', {ft: ft, ts: ts} );
+
+    setTimeout(sendSample, 20);
 }
