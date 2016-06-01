b779b1f9a346969e5de710675ab7280eeef2920b
Michael Bromley <michael@michaelbromley.co.uk>
2016-06-01 22:12:52 +0200

    Start re-designing settings panel

---
+++ src/render/components/settings-modal/settings-modal.component.ts
@@ -0,0 +1,15 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+
+@Component({
+    selector: 'settings-modal',
+    template: require('./settings-modal.component.html'),
+    styles: [
+        `:host { position: absolute; }`,
+        require('./settings-modal.scss').toString()
+    ]
+})
+export class SettingsModal { 
+    private icon_close = require('../../../assets/icons/cross.svg');
+    @Input() visible: boolean;
+    @Output() close = new EventEmitter<boolean>();
+}
--- src/render/components/settings-panel/settings-panel.component.ts
+++ src/render/components/settings-panel/settings-panel.component.ts
@@ -1,29 +1,54 @@
-import {Component, Input, EventEmitter, Output} from '@angular/core';
+import {Component, HostListener, Input, EventEmitter, Output} from '@angular/core';
 import {InputSelector} from '../input-selector/input-selector.component';
-import {VSelector} from '../v-selector/v-selector.component';
-import {ParameterControls} from '../parameter-controls/parameter-controls.component';
-import {IVisualization, IParamUpdate} from '../../../common/models';
- 
+import {SettingsModal} from '../settings-modal/settings-modal.component';
+  
 @Component({
     selector: 'settings-panel',
     template: require('./settings-panel.component.html'),
     styles: [require('./settings-panel.scss').toString()],
-    directives: [InputSelector, VSelector, ParameterControls]
+    directives: [SettingsModal, InputSelector]
 })
 export class SettingsPanel {
     @Input() inputDevices:  { [id: number]: string } = {}; 
     @Input() library: { id: number; name: string }[];
-    @Input() libraryDir: string; 
-    @Input() currentVis: IVisualization;
+    @Input() libraryDir: string;
     @Output() changeInputDeviceId = new EventEmitter<number>();
-    @Output() selectVisId = new EventEmitter<number>(); 
     @Output() selectLibraryDir = new EventEmitter<boolean>();
-    @Output() updateParam = new EventEmitter<IParamUpdate>();
-    @Output() changeVisibility = new EventEmitter<boolean>();
-    private isVisible: boolean = false;
-    
-    toggleVisibility() {
-        this.isVisible = !this.isVisible;
-        this.changeVisibility.emit(this.isVisible);
+    private iconsVisible: boolean = false;
+    private modalVisible: boolean = false;
+    private icon_inputs = require('../../../assets/icons/power-cord.svg');
+    private icon_info = require('../../../assets/icons/info.svg');
+    private icon_vol_mute = require('../../../assets/icons/volume-mute.svg');
+    private icon_vol_low = require('../../../assets/icons/volume-low.svg');
+    private icon_vol_medium = require('../../../assets/icons/volume-medium.svg');
+    private icon_vol_high = require('../../../assets/icons/volume-high.svg');
+
+
+    @HostListener('document:mouseover')
+    showIcons(): void {
+        if (!this.modalVisible && !this.iconsVisible) {
+            this.iconsVisible = true;
+        }
+    }
+
+    @HostListener('document:mouseout') 
+    hideIcons(): void {
+        if (this.iconsVisible) {
+            this.iconsVisible = false;
+        } 
+    }
+
+    showModal() {
+        this.modalVisible = true;
+        this.hideIcons();
+    }
+
+    hideModal() {
+        this.modalVisible = false;
+        this.showIcons();
+    }
+
+    getVolumeIcon(): string {
+        return this.icon_vol_medium;
     }
 } 
